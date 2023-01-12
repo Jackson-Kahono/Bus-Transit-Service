007dc4eb0ed80eeac6145868831cba6eaaa34659
Esther Maina <71544572+EstherWMaina@users.noreply.github.com>
2023-01-12 14:02:29 +0300

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :mpesas
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #stkpush route
-  post 'stkpush', to: 'mpesas#stkpush'
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post "/stkpush", to: "mpesas#stkpush"
+  post "/stkquery", to: "mpesas#stkquery"
+
+
 end
