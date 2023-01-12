6c192553587794098c57d9671c1a61b391cf1c18
Esther Maina <71544572+EstherWMaina@users.noreply.github.com>
2023-01-12 12:43:54 +0300

    Added MPesa stk files

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :mpesas
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #stkpush route
+  post 'stkpush', to: 'mpesas#stkpush'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
